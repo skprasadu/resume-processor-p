fd7ddccbbdbf58aa6153a58cdc728f0e3f12b229
K P <k@p.com>
2019-06-15 16:28:50 -0700

    added werkzeug

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from wordcloud import WordCloud,STOPWORDS
 from flask import Flask, request, Response, render_template
 import textract
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
@@ -21,7 +22,8 @@ def upload():
 
     file = request.files['file']
     if file:
-        text = textract.process(file.read())
+		filename = secure_filename(file.filename)
+        text = textract.process(filename)
         wordcloud = wc.generate(text)
         img = BytesIO()
         wordcloud.to_image().save(img, 'PNG')
