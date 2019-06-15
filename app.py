67dc19432dcf391055390429045b239ded15b966
K P <k@p.com>
2019-06-15 15:11:58 -0700

    added / route

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ from flask import Flask
 from wordcloud import WordCloud,STOPWORDS
 from flask import request, Response, render_template
 import textract
+
+@app.route('/')
+def index():
+    return 'hello'
